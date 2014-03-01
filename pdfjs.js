f03bd8742590a4c09db17cd4416e36561863a067
rkusa <m@rkusa.st>
2014-03-01 13:13:39 +0100

    improve browserify compatiblity

--- pdfjs.js
+++ pdfjs.js
@@ -1043,7 +1043,9 @@ var Font = module.exports = function(opts) {
   this.subsets = {}
   TYPES.forEach(function(type) {
     if (type in opts) {
-      if (!(opts[type] instanceof ArrayBuffer) && !(opts[type] instanceof Buffer))
+      // GLOBAL['Buffer'] is used instead of Buffer to trick browserify
+      // to not load a Buffer polyfill just for instance testing
+      if (!(opts[type] instanceof ArrayBuffer) && !(typeof GLOBAL !== 'undefined' && opts[type] instanceof GLOBAL['Buffer']))
         throw new Error('Property `' + type + '` must be a Buffer or a Arraybuffer.')
       self[type] = new TTFFont(opts[type])
       self.subsets[type] = self[type].subset()
