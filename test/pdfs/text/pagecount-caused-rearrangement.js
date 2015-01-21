3b21fa04e0c646806877d1667a4e167d3e2cf75c
rkusa <m@rkusa.st>
2015-01-21 09:35:10 +0100

    fix page count caused rearrangement

--- test/pdfs/text/pagecount-caused-rearrangement.js
+++ test/pdfs/text/pagecount-caused-rearrangement.js
@@ -1,12 +1,12 @@
 module.exports = function(doc, fixtures) {
   var text = doc.text()
-  text.line(fixtures.lorem.long + '\n')
+  text.line(fixtures.lorem.long + '\n\n')
 
   text.add('......................................................................................................................................................................................................')
   text.pageCount() //{ font: fixtures.font.opensans.bold })
   text.br().br()
 
   for (var i = 0; i < 26; ++i) {
-    text.line(fixtures.lorem.long + '\n')
+    text.line(fixtures.lorem.long + '\n\n')
   }
 }
