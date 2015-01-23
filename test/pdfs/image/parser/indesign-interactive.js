22e6b48e06208f087f3827f622e7d1abbc26cd47
rkusa <m@rkusa.st>
2015-01-23 08:38:12 +0100

    add more PDF embedding fixes and tests

---
+++ test/pdfs/image/parser/indesign-interactive.js
@@ -0,0 +1,15 @@
+var pdfjs = require('../../../../')
+var fs = require('fs')
+
+module.exports = function(doc, fixtures) {
+  doc.style = doc.style.merge({ paddingTop: 100 })
+
+  var img = pdfjs.createImage(fs.readFileSync(__dirname + '/fixtures/indesign-interactive.pdf'))
+
+  doc.image(img, {
+    wrap: false, x: 0, y: 0, width: 595.276, height: 841.89
+  })
+
+  var text = doc.text({ textAlign: 'justify' })
+  text.add(fixtures.lorem.short)
+}
