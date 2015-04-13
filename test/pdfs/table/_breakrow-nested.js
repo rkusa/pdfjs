4e37c3790a9e4900f05e76ee1297c1f51daef048
rkusa <m@rkusa.st>
2015-04-13 12:49:33 +0200

    fix page breaks

---
+++ test/pdfs/table/_breakrow-nested.js
@@ -0,0 +1,34 @@
+module.exports = function(doc, fixtures) {
+
+  var table = doc.table({
+    padding: 5,
+    borderWidth: 1,
+    borderColor: 0xdddddd,
+    backgroundColor: 0xeeeeee,
+    widths: ['100%']
+  })
+
+  var tr1 = table.tr({})
+  tr1.td(fixtures.lorem.long + fixtures.lorem.long + fixtures.lorem.short, { height: 600 })
+
+  var tr2 = table.tr({ allowBreak: true })
+
+  var table2 = tr2.td({ allowBreak: true }).table({
+    padding: 5,
+    borderWidth: 1,
+    borderColor: 0xdddddd,
+    backgroundColor: 0xeeeeee,
+    widths: ['50%', '50%'],
+    headerRows: 2
+  })
+
+  var beforeBreak = table2.beforeBreak()
+  beforeBreak.td('A\nA\nA')
+  beforeBreak.td('B\nB\nB')
+
+  for (var i = 0; i < 10; ++i) {
+    var tr = table2.tr()
+    tr.td(i)
+    tr.td(i)
+  }
+}
