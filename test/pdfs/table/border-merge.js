4b7004295a06a0ab37ea50ed3916b22eec79512c
rkusa <m@rkusa.st>
2015-05-20 15:40:53 +0200

    fix cell border merging
    Refs #25

--- test/pdfs/table/border-merge.js
+++ test/pdfs/table/border-merge.js
@@ -1,17 +1,32 @@
 module.exports = function(doc, fixtures) {
-  var table = doc.table({
+  var table1 = doc.table({
     padding: 5,
     borderWidth: 20,
     borderColor: 0xdddddd,
     backgroundColor: 0xeeeeee,
-    widths: ['50%', '30%']
+    widths: ['50%', '50%']
   })
 
-  var tr1 = table.tr({})
+  var tr1 = table1.tr({})
   tr1.td(fixtures.lorem.short, { borderRightWidth: 50 })
   tr1.td(fixtures.lorem.short)
 
-  var tr2 = table.tr({})
+  var tr2 = table1.tr({})
   tr2.td(fixtures.lorem.short, { borderTopWidth: 60 })
+  tr2.td(fixtures.lorem.short)
+
+  var table2 = doc.table({
+    padding: 5,
+    borderColor: 0xdddddd,
+    backgroundColor: 0xeeeeee,
+    widths: ['50%', '50%']
+  })
+
+  tr1 = table2.tr({ borderBottomWidth: 10 })
+  tr1.td(fixtures.lorem.short)
+  tr1.td(fixtures.lorem.short)
+
+  tr2 = table2.tr({ borderBottomWidth: 5 })
+  tr2.td(fixtures.lorem.short)
   tr2.td(fixtures.lorem.short)
 }
