0c27b1b974264715f1b946262d5bfdca29b0d9ca
rkusa <m@rkusa.st>
2015-05-22 20:08:11 +0200

    fix relative operations inside tables
    Refs #27

--- test/pdfs/operation.js
+++ test/pdfs/operation.js
@@ -16,6 +16,23 @@ module.exports = function(doc, fixtures) {
     return [x, y - height, x + 60, height, 're']
   })
   ops.op('f')
+
+  // inside table
+  var table = doc.table({ padding: 10, widths: ['20%', '10%'] })
+
+  for (var i = 0; i < 2; ++i) {
+    var tr = table.tr()
+    tr.td('Test')
+
+    var height = 40
+    var td = tr.td({ height: height })
+    ops = td.ops()
+    ops.op(1, 0, 0, 'rg')
+    ops.op(function(x, y) {
+      return [x, y - height, x + 60, height, 're']
+    })
+    ops.op('f')
+  }
 }
 
 
