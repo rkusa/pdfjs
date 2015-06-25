638df4c21c6a3b7518471f9014289a64b72e5134
rkusa <m@rkusa.st>
2015-06-25 14:34:42 +0200

    fix page count inside header/footer
    Refs #34

--- test/pdfs/text/pagecount.js
+++ test/pdfs/text/pagecount.js
@@ -1,15 +1,13 @@
 module.exports = function(doc, fixtures) {
+  doc.footer().text()
+     .pageNumber().add('/').pageCount().br()
+     .pageNumber().append('/').appendPageCount()
+
   var text = doc.text()
 
   for (var j = 0; j < 3; ++j) {
-    if (j > 0) {
-      for (var i = 0; i < 70; ++i) {
-        text.br()
-      }
+    for (var i = 0; i < 70; ++i) {
+      text.add('-').br()
     }
-
-
-    text.pageNumber().add('/').pageCount().br()
-        .pageNumber().append('/').appendPageCount()
   }
 }
