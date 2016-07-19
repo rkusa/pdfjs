f6c715faf80c0530755ae4b03faa4cdfdfb0f294
Markus Ast <m@rkusa.st>
2016-07-19 10:12:10 +0200

    fix embedding of itext2 pdfs
    Refs #57

---
+++ test/pdfs/image/parser/itext7.js
@@ -0,0 +1,16 @@
+var pdfjs = require('../../../../')
+var path = require('path')
+var fs = require('fs')
+
+module.exports = function(doc, fixtures) {
+  doc.style = doc.style.merge({ paddingTop: 100 })
+
+  var img = pdfjs.createImage(fs.readFileSync(path.join(__dirname, 'fixtures/itext7.pdf')))
+
+  doc.image(img, {
+    wrap: false, x: 0, y: 0, width: 595.2, height: 841.92
+  })
+
+  var text = doc.text({ textAlign: 'justify' })
+  text.add(fixtures.lorem.short)
+}
