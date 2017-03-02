6d6e392f7f0a6bcec6c32365b294296e551676ca
Markus Ast <m@rkusa.st>
2017-03-02 07:37:56 +0100

    implement page numbers

---
+++ test/pdfs/headerfooter/pagenumber.js
@@ -0,0 +1,17 @@
+module.exports = function(doc) {
+  const footer = doc.footer()
+  footer.text('before', { textAlign: 'center' })
+  footer.pageNumber({ textAlign: 'center', fontSize: 16 })
+  footer.text('after', { textAlign: 'center' })
+
+  const header = doc.header()
+  header.text('before', { textAlign: 'center' })
+  header.pageNumber({ textAlign: 'center', fontSize: 16 })
+  header.text('after', { textAlign: 'center' })
+
+  doc.text('Hello World 1')
+
+  doc.pageBreak()
+
+  doc.text('Hello World 2')
+}
