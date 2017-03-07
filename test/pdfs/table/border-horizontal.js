2d78418590455d3c708490a02b3e993303fef0d2
Markus Ast <m@rkusa.st>
2017-03-07 08:34:59 +0100

    improve table borders and add table border tests

---
+++ test/pdfs/table/border-horizontal.js
@@ -0,0 +1,34 @@
+module.exports = function(doc, { lorem })  {
+  {
+    const table = doc.table({
+      backgroundColor: 0xeeeeee,
+      widths: [256, 256],
+      borderHorizontalWidth: 10
+    })
+
+    for (let i = 0; i < 3; ++i) {
+      const row = table.row()
+
+      row.cell('Left ' + i, { fontSize: 20, padding: 10 })
+      row.cell('Right ' + i, { fontSize: 20, padding: 10 })
+    }
+  }
+
+  doc.text('–––––')
+
+  {
+    const table = doc.table({
+      backgroundColor: 0xeeeeee,
+      widths: [256, 256],
+      borderHorizontalWidths: i => (i + 1) * 5
+    })
+
+    for (let i = 0; i < 3; ++i) {
+      const row = table.row()
+
+      row.cell('Left ' + i, { fontSize: 20, padding: 10 })
+      row.cell('Right ' + i, { fontSize: 20, padding: 10 })
+    }
+  }
+}
+
---
+++ test/pdfs/table/border.js
@@ -0,0 +1,13 @@
+module.exports = function(doc, { lorem })  {
+  const table = doc.table({
+    backgroundColor: 0xeeeeee,
+    widths: [256, 256],
+    borderWidth: 10,
+  })
+
+  const row = table.row()
+
+  row.cell(lorem.shorter, { textAlign: 'justify', fontSize: 20, padding: 10 })
+  row.cell(lorem.shorter, { textAlign: 'justify', fontSize: 20, padding: 10 })
+}
+
