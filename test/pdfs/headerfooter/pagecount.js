adcacdcf607e16fb3bd381d28b038736f381fdaf
Markus Ast <m@rkusa.st>
2017-03-10 21:05:04 +0100

    fix page number positioning

--- test/pdfs/headerfooter/pagecount.js
+++ test/pdfs/headerfooter/pagecount.js
@@ -1,13 +1,11 @@
 module.exports = function(doc) {
   const footer = doc.footer()
-  footer.text('before', { textAlign: 'center' })
   footer.pageNumber((curr, total) => `${curr} / ${total}`, { textAlign: 'center', fontSize: 16 })
   footer.text('after', { textAlign: 'center' })
 
   const header = doc.header()
   header.text('before', { textAlign: 'center' })
   header.pageNumber((curr, total) => `${curr} / ${total}`, { textAlign: 'center', fontSize: 16 })
-  header.text('after', { textAlign: 'center' })
 
   doc.text('Hello World 1')
 
--- test/pdfs/headerfooter/pagenumber.js
+++ test/pdfs/headerfooter/pagenumber.js
@@ -2,10 +2,8 @@ module.exports = function(doc) {
   const footer = doc.footer()
   footer.text('before', { textAlign: 'center' })
   footer.pageNumber({ textAlign: 'center', fontSize: 16 })
-  footer.text('after', { textAlign: 'center' })
 
   const header = doc.header()
-  header.text('before', { textAlign: 'center' })
   header.pageNumber({ textAlign: 'center', fontSize: 16 })
   header.text('after', { textAlign: 'center' })
 
