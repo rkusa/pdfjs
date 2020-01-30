c0135b3ea4adda249fe2c4992fc02ed34c7fe8f3
Markus Ast <m@rkusa.st>
2020-01-30 11:39:05 +0100

    fix various table break issues Refs #191

---
+++ test/pdfs/issue/issue-191-row-breaks.js
@@ -0,0 +1,32 @@
+module.exports = function(doc) {
+  doc.cell({ y: 140 })
+
+  const table = doc.table({
+    widths: [100, null],
+    borderWidth: 1,
+    borderColor: 0xf00f00,
+  });
+  const row = table.row();
+
+  row.cell(`heading`, {
+    padding: 12,
+  });
+
+  const cell = row.cell();
+
+  cell.cell(`value - 1`, {
+    padding: 12,
+  });
+
+  cell.cell(`value - 2`, {
+    padding: 10,
+  });
+
+  cell.cell(`value - 3`, {
+    padding: 10,
+  });
+
+  cell.cell(`value - 4`, {
+    padding: 10,
+  });
+}
