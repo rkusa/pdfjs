616375761d3da5537f1ca2a410e3f2eb12e279e3
KindaOK <27974340+KindaOK@users.noreply.github.com>
2022-11-28 20:37:33 -0500

    Fix for table header border indexing
    Table header rows would skip an index when a table with exclusively headers is rendered.

---
+++ test/pdfs/table/header-border.js
@@ -0,0 +1,28 @@
+module.exports = function(doc, { lorem })  {
+
+  // borders should be on the odd horizontal lines
+  const odd_borders = doc.table({
+    widths: [null, null],
+    borderHorizontalWidths: i => (i % 2) * 5,
+    borderColor: 0x00ff00,
+  })
+  for (let i = 0; i < 3; ++i) {
+    const header = odd_borders.header()
+    header.cell('Odd Left ' + i, { textAlign: 'center', padding: 30, backgroundColor: 0xff0000 })
+    header.cell('Odd Right ' + i, { textAlign: 'center', padding: 30, backgroundColor: 0xff0000 })
+
+  }
+
+  // borders should be on the odd horizontal lines
+  const even_borders = doc.table({
+    widths: [null, null],
+    borderHorizontalWidths: i => ((i+1) % 2) * 5,
+    borderColor: 0x0000ff,
+  })
+  for (let i = 0; i < 3; ++i) {
+    const header = even_borders.header()
+    header.cell('Even Left ' + i, { textAlign: 'center', padding: 30 , backgroundColor: 0xff0000 })
+    header.cell('Even Right ' + i, { textAlign: 'center', padding: 30 , backgroundColor: 0xff0000 })
+  }
+}
+
