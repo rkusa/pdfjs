b46b431ee0f56afb18b1c0be1e29ca85807aa7f3
Tim Weißenfels <10491533+tim-we@users.noreply.github.com>
2023-12-05 10:36:22 +0100

    Fix Document.asBuffer types

--- types/main.d.ts
+++ types/main.d.ts
@@ -91,7 +91,20 @@ declare module "pdfjs" {
          * Can be used to render the document as a buffer
          * @param callback callback called with either error or buffer
          */
-        asBuffer(callback?: (err: Error, data: Buffer) => void): void;
+        asBuffer(callback: (err: Error, data: Buffer) => void): Promise<unknown>;
+
+        /**
+         * Can be used to render the document as a buffer
+         * @param opts can be used to control if the document will automatically be ended
+         */
+        asBuffer(opts: AsBufferOptions): Promise<Buffer>;
+
+        /**
+         * Can be used to render the document as a buffer
+         * @param opts can be used to control if the document will automatically be ended
+         * @param callback callback called with either error or buffer
+         */
+        asBuffer(opts: AsBufferOptions, callback: (err: Error, data: Buffer) => void): Promise<unknown>;
 
         /**
          * Add a header to the document
@@ -131,6 +144,8 @@ declare module "pdfjs" {
          */
         outline(title: string, destination: string, parent?: string | number): void;
     }
+
+    type AsBufferOptions = { end: boolean };
 
     export class Fragment {
         end(): void; // TODO
